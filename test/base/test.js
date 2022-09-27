17909214ff07ca09d227f2cd10d92d7b7ea61f7f
Alex Komoroske <jkomoros@gmail.com>
2022-09-26 18:40:17 -1000

    Added more basic tests.
    Part of #1.

--- test/base/test.js
+++ test/base/test.js
@@ -6,6 +6,24 @@ import {
 
 import assert from 'assert';
 
+const legalBaseInput = {
+	"version": 1,
+	"types": {
+		"engineering": {
+			"value": 3,
+			"weight": 1.0
+		}
+	},
+	"root": {
+		"engineering" : 4.0
+	},
+	"nodes": {
+		"a": {
+			"description": "Node a"
+		}
+	}
+};
+
 describe('AdjacencyMap', () => {
 	it('barfs on null data', async () => {
 		const input = null;
@@ -18,5 +36,44 @@ describe('AdjacencyMap', () => {
 		} else {
 			assert.doesNotThrow(fn);
 		}
-	}); 
+	});
+
+	it('barfs on no nodes', async () => {
+		const input = Object.fromEntries(Object.entries(legalBaseInput).filter(entry => entry[0] != 'nodes'));
+		const errorExpected = true;
+		const fn = () => {
+			new AdjacencyMap(input);
+		};
+		if (errorExpected) {
+			assert.throws(fn);
+		} else {
+			assert.doesNotThrow(fn);
+		}
+	});
+
+	it('barfs on undefined types', async () => {
+		const input = Object.fromEntries(Object.entries(legalBaseInput).filter(entry => entry[0] != 'types'));
+		const errorExpected = true;
+		const fn = () => {
+			new AdjacencyMap(input);
+		};
+		if (errorExpected) {
+			assert.throws(fn);
+		} else {
+			assert.doesNotThrow(fn);
+		}
+	});
+
+	it('barfs on empty types', async () => {
+		const input = {...legalBaseInput, types: {}};
+		const errorExpected = true;
+		const fn = () => {
+			new AdjacencyMap(input);
+		};
+		if (errorExpected) {
+			assert.throws(fn);
+		} else {
+			assert.doesNotThrow(fn);
+		}
+	});
 });
