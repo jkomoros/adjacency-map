c8d414e440529fcbdaa9ba2e557b903cd6c83601
Alex Komoroske <jkomoros@gmail.com>
2022-09-30 09:32:30 -0700

    Moved the isTrue and friends to constants.
    Part of #3.

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,18 @@ import {
 } from './types.js';
 
 export const ROOT_ID : NodeID = '';
+
+/*
+
+	ValueDefinitions exclusively work with numbers, so to represent true/false
+	we need a convention.
+
+*/
+
+//Note: any non-false number value is true, so use isTrue to check if a given
+//number is true.
+export const DEFAULT_TRUE_NUMBER = 1.0;
+export const FALSE_NUMBER = 0.0;
+
+export const isTrue = (input : number) => !isFalse(input);
+export const isFalse = (input : number) => input == FALSE_NUMBER;
