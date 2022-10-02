250f4dd64621761d5806b79dde8c1ba4bd78486e
Alex Komoroske <jkomoros@gmail.com>
2022-10-02 08:20:00 -0700

    Remove old code from `tools/config.ts` from CASsim.
    Part of #7. Part of #8.

--- tools/config.ts
+++ tools/config.ts
@@ -1,19 +1,11 @@
 import * as fs from "fs";
 import * as path from "path";
 
-const SIMULATORS_DIR = 'src/simulators/';
 const DYNAMIC_TYPES_FILE = 'src/types-dynamic.GENERATED.ts';
 const DATA_DIRECTORY = 'data';
 
 const generateConfig = () => {
-	const simulatorNames = [];
 	const datafiles = [];
-	for (const simulator of fs.readdirSync(SIMULATORS_DIR)) {
-		if (!simulator.endsWith('.ts')) continue;
-		if (simulator.endsWith('.d.ts')) continue;
-		const filename = path.basename(simulator, '.ts');
-		simulatorNames.push(filename);
-	}
 	for (const file of fs.readdirSync(DATA_DIRECTORY)) {
 		const filename = path.basename(file, '.json');
 		datafiles.push(filename);
@@ -22,11 +14,7 @@ const generateConfig = () => {
 
 //All of these exports are also re-exported from src/types.ts, so you should import them from there.
 
-export const KNOWN_DATA_FILES = ${JSON.stringify(datafiles, null, '\t').split('"').join("'")};
-
-export type SimulatorType = '' | ${simulatorNames.map(item => '\'' + item + '\'').join(' | ')};
-
-export const KNOWN_SIMULATOR_TYPES : SimulatorType[] = ${JSON.stringify(simulatorNames, null, '\t').split('"').join("'")};`;
+export const KNOWN_DATA_FILES = ${JSON.stringify(datafiles, null, '\t').split('"').join("'")};`;
 
 	fs.writeFileSync(DYNAMIC_TYPES_FILE, data);
 };
