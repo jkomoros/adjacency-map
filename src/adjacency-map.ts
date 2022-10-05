24e433ae20e8fb3c03153ff3b7c94195e8ff2d16
Alex Komoroske <jkomoros@gmail.com>
2022-10-04 17:07:29 -0700

    Use wrapArrays to make the various return values of edges the same length in _calculateRenderEdges.
    Nothing changes yet.
    
    Part of #4.

--- src/adjacency-map.ts
+++ src/adjacency-map.ts
@@ -60,6 +60,7 @@ import {
 	color,
 	unpackColor
 } from './color.js';
+import { wrapArrays } from './util.js';
 
 export const extractSimpleGraph = (data : MapDefinition) : SimpleGraph => {
 	const result : SimpleGraph = {};
@@ -503,8 +504,10 @@ class AdjacencyMapNode {
 				const distinctDefinition = edgeDefinition.display.distinct || this._map.data.display.edge.distinct;
 				const distincts = this._edgeDefinitionHelper(distinctDefinition, edges);
 
+				const [wrappedColors, wrappedWidths, wrappedOpacities, wrappedDistincts] = wrapArrays(colors, widths, opacities, distincts);
+
 				//TODO: remove. This is just here to get it to compile
-				console.info(colors, widths, opacities, distincts);
+				console.info(wrappedColors, wrappedWidths, wrappedOpacities, wrappedDistincts);
 
 				for (const edge of edges) {
 					if (edgeType == 'just putting this here to compile') continue;
