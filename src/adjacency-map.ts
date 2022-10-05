e91b369d6748f6e933afb655dcafa0ee07871fcd
Alex Komoroske <jkomoros@gmail.com>
2022-10-05 10:52:53 -0700

    Move BASE_*_DISPLAY into libraries.ts
    Part of #4.

--- src/adjacency-map.ts
+++ src/adjacency-map.ts
@@ -56,7 +56,10 @@ import {
 
 import {
 	CORE_LIBRARY_NAME,
-	LIBRARIES
+	LIBRARIES,
+	BASE_NODE_DISPLAY,
+	BASE_EDGE_DISPLAY,
+	BASE_EDGE_COMBINER_DISPLAY
 } from './libraries.js';
 
 import {
@@ -81,37 +84,6 @@ export const extractSimpleGraph = (data : MapDefinition) : SimpleGraph => {
 	}
 	result[ROOT_ID] = {};
 	return result;
-};
-
-const BASE_NODE_DISPLAY : NodeDisplay = {
-	radius: 6,
-	opacity: 1.0,
-	color: {
-		color: '#333'
-	}
-};
-
-//Since none of these return an array of numbers, edges of the same type from
-//the same ref/source pair will be combined together. If you want them to not
-//combine, override one of these to return {lengthOf:'edges', value: FOO}
-const BASE_EDGE_DISPLAY : EdgeDisplay = {
-	width: 1.5,
-	color: {
-		color: '#555'
-	},
-	opacity: 0.4,
-	distinct: false
-};
-
-const BASE_EDGE_COMBINER_DISPLAY : EdgeCombinerDisplay = {
-	width: {
-		combine: 'sum',
-		value: 'input'
-	},
-	color: {
-		color: '#555'
-	},
-	opacity: 0.4
 };
 
 //Does things like include libraries, convert Raw* to * (by calculateValueLeaf
--- src/libraries.ts
+++ src/libraries.ts
@@ -1,9 +1,43 @@
 import {
+	EdgeCombinerDisplay,
+	EdgeDisplay,
 	Library,
-	LibraryType
+	LibraryType,
+	NodeDisplay
 } from './types.js';
 
 export const CORE_LIBRARY_NAME = 'core';
+
+export const BASE_NODE_DISPLAY : NodeDisplay = {
+	radius: 6,
+	opacity: 1.0,
+	color: {
+		color: '#333'
+	}
+};
+
+//Since none of these return an array of numbers, edges of the same type from
+//the same ref/source pair will be combined together. If you want them to not
+//combine, override one of these to return {lengthOf:'edges', value: FOO}
+export const BASE_EDGE_DISPLAY : EdgeDisplay = {
+	width: 1.5,
+	color: {
+		color: '#555'
+	},
+	opacity: 0.4,
+	distinct: false
+};
+
+export const BASE_EDGE_COMBINER_DISPLAY : EdgeCombinerDisplay = {
+	width: {
+		combine: 'sum',
+		value: 'input'
+	},
+	color: {
+		color: '#555'
+	},
+	opacity: 0.4
+};
 
 export const LIBRARIES : {[type in LibraryType] : Library} = {
 	core: {
