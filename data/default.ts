06807074dbeac32a087386e8d6e0e9d59590e3a7
Alex Komoroske <jkomoros@gmail.com>
2022-10-07 12:29:09 -0700

    Start having a made-up scenario around augmented reality as a demonstration.
    Part of #11.

--- data/default.ts
+++ data/default.ts
@@ -5,60 +5,132 @@ import {
 const data : RawMapDefinition = {
 	properties: {
 		engineering: {
-			value: 3,
+			description: 'The amount of engineering effort to build to this use case',
+			value: {
+				operator: '+',
+				a: {
+					ref: 'engineering'
+				},
+				b: {
+					constant: 'weight'
+				}
+			},
+			display: {
+				color: 'red'
+			},
+			implies: '*',
 			constants: {
 				weight: 1.0
 			}
 		},
 		ux: {
-			value: 4,
-			description: "A description of ux"
-		}
-	},
-	root: {
-		engineering : 4.0
+			description: 'The amount of ux effort to build to this use case',
+			value: {
+				operator: '+',
+				a: {
+					ref: 'ux'
+				},
+				b: {
+					constant: 'weight'
+				}
+			},
+			display: {
+				color: 'blue'
+			},
+			implies: '*',
+			constants: {
+				weight: 1.0
+			}
+		},
+		data: {
+			description: 'The amount of effort to build up data quality to build to this use case',
+			value: {
+				operator: '+',
+				a: {
+					ref: 'data'
+				},
+				b: {
+					constant: 'weight'
+				}
+			},
+			display: {
+				color: 'green'
+			},
+			implies: '*',
+			constants: {
+				weight: 1.0
+			}
+		},
+		usage: {
+			description: 'The amount of effort required to modify usage behaviors and expectations activate this use case',
+			value: {
+				operator: '+',
+				a: {
+					ref: 'usage'
+				},
+				b: {
+					constant: 'weight'
+				}
+			},
+			display: {
+				color: 'yellow'
+			},
+			implies: '*',
+			constants: {
+				weight: 1.0
+			}
+		},
+
 	},
 	nodes: {
-		a: {
-			description: "Node a",
+		orient: {
+			description: 'A basic orientation calibration experience',
 			edges: [
 				{
-					type: "engineering",
+					type: 'engineering',
+					weight: 6.0
+				},
+				{
+					type: 'ux',
 					weight: 4.0
 				},
 				{
-					type: "engineering"
+					type: 'data',
+					weight: 4.0
 				}
 			]
 		},
-		b: {
-			description: "Node b",
+		compass: {
+			description: 'Once oriented, display compass rose',
 			edges: [
 				{
-					type: "ux",
-					ref: "a"
-				}
-			]
-		},
-		c: {
-			description: "Node c",
-			edges: [
-				{
-					type: "engineering",
-					ref: "a"
+					type: 'engineering',
+					ref: 'orient',
+					weight: 1.0
 				},
 				{
-					type: "ux",
-					ref: "b"
+					type: 'ux',
+					weight: 1.0
 				}
 			]
 		},
-		d: {
-			description: "Node d",
+		'landmarks': {
+			description: 'Overlay distant land marks to help orient',
 			edges: [
 				{
-					type: "engineering",
-					ref: "b"
+					type: 'engineering',
+					ref: 'compass',
+					weight: 2.0
+				},
+				{
+					type: 'ux',
+					ref: 'compass',
+					weight: 2.0
+				},
+				{
+					type: 'data',
+					ref: 'compass',
+					weight: 1.0
 				}
 			]
 		}
