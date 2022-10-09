a28fc7934b9a7aa4f8c3ec8f8ee1a0b3e9cbd50b
Alex Komoroske <jkomoros@gmail.com>
2022-10-09 14:26:55 -0700

    Make it so when different nodes are hovered in SVG, the hovered nodeID is updated.
    Part of #22.

--- src/components/main-view.ts
+++ src/components/main-view.ts
@@ -10,6 +10,7 @@ import {
 	nextScenarioName,
 	previousScenarioName,
 	updateFilename,
+	updateHoveredNodeID,
 	updateScale,
 	updateScenarioName,
 	updateWithMainPageExtra
@@ -259,6 +260,13 @@ class MainView extends connect(store)(PageViewElement) {
 		return result;
 	}
 
+	_handleSVGMouseMove(e : MouseEvent) {
+		const ele = e.composedPath()[0];
+		if (!(ele instanceof Element)) throw new Error('unexpected node');
+		const id = ele.id.includes('node:') ? ele.id.replace('node:', '') : undefined;
+		store.dispatch(updateHoveredNodeID(id));
+	}
+
 	_svg() : TemplateResult {
 		if (!this._adjacencyMap) return svg``;
 
@@ -273,13 +281,13 @@ class MainView extends connect(store)(PageViewElement) {
 		// padding around the labels
 		const haloWidth = 3;
 
-		return html`<svg class='main' viewBox='${a.viewBox}' width='${a.width * this._scale}' height='${a.height * this._scale}' style='max-width: 100%; height: auto; height: intrinsic;' font-family='sans-serif' font-size='10'>
+		return html`<svg @mousemove=${this._handleSVGMouseMove} class='main' viewBox='${a.viewBox}' width='${a.width * this._scale}' height='${a.height * this._scale}' style='max-width: 100%; height: auto; height: intrinsic;' font-family='sans-serif' font-size='10'>
 			<g fill="none" stroke-linecap="${strokeLinecap}" stroke-linejoin="${strokeLinejoin}">
 				${a.renderEdges.map(edge => svg`<path d="${this._pathForEdge(edge, a)}" stroke-width='${edge.width}' stroke-opacity='${edge.opacity}' stroke='${edge.color.rgbaStr}'></path>`)}
 			</g>
 			<g>
 				${Object.values(a.nodes).map(node => svg`<a transform="translate(${node.x},${node.y})">
-					<circle fill="${node.color.rgbaStr}" r="${node.radius}" opacity="${node.opacity}" stroke="${node.strokeColor.rgbaStr}" stroke-width="${node.strokeWidth}" stroke-opacity="${node.strokeOpacity}"></circle>
+					<circle @mousemove=${this._handleSVGMouseMove} id="${'node:' + node.id}" fill="${node.color.rgbaStr}" r="${node.radius}" opacity="${node.opacity}" stroke="${node.strokeColor.rgbaStr}" stroke-width="${node.strokeWidth}" stroke-opacity="${node.strokeOpacity}"></circle>
 					<title>${node.fullDescription()}</title>
 					<text dy="0.32em" x="${(node.children.length == 0 ? 1 : -1) * node.radius * 2}" text-anchor="${node.children.length == 0 ? 'start' : 'end'}" paint-order="stroke" stroke="${halo}" stroke-width="${haloWidth}">${node.displayName}</text>
 				</a>`)}
