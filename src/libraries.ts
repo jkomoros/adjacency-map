jkomoros/adjacency-map

commit 26d34d20a4aa7cb9a4de5c12d91d2d8d3a016a57

Make it so in the `product` library, the opacity of the lines is the certainty of the result node they connect to.
Part of #11.

src/libraries.ts
@@ -30,7 +30,9 @@ export const BASE_EDGE_DISPLAY : EdgeDisplay = {
 	color: {
 		color: '#555'
 	},
-	opacity: 0.4,
+	opacity: {
+		result: 'certainty'
+	},
 	distinct: false
 };
 

commit e2d9fb1cef760c7d14978670130901957f5e279f

Fix a bug introduced in 26d34d20a4aa7cb9a4de5c12d91d2d8d3a016a57 where definitions that didn't use the product library would fail to validate.
The edge opacity definition went in the base defintion, instead of in product.

Note that a number of tests still fail because edfcad5db303e10a611662d47e8314d037c46103 also broke a few
tests which weren't noticed until now.

Part of #11.

src/libraries.ts
@@ -30,9 +30,7 @@ export const BASE_EDGE_DISPLAY : EdgeDisplay = {
 	color: {
 		color: '#555'
 	},
-	opacity: {
-		result: 'certainty'
-	},
+	opacity: 0.4,
 	distinct: false
 };
 
@@ -62,6 +60,9 @@ const baseProductPropertyDefinition = {
 		color: 'red',
 		width: {
 			constant: 'cost'
+		},
+		opacity: {
+			result: 'certainty'
 		},
 		distinct: true
 	},