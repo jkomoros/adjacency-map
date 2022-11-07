jkomoros/adjacency-map

commit e2d9fb1cef760c7d14978670130901957f5e279f

Fix a bug introduced in 26d34d20a4aa7cb9a4de5c12d91d2d8d3a016a57 where definitions that didn't use the product library would fail to validate.
The edge opacity definition went in the base defintion, instead of in product.

Note that a number of tests still fail because edfcad5db303e10a611662d47e8314d037c46103 also broke a few
tests which weren't noticed until now.

Part of #11.

src/libraries.ts
@@ -30,9 +30,7 @@ export const BASE_EDGE_DISPLAY : EdgeDisplay = {
 	color: {
 		color: '#555'
 	},
-	opacity: {
-		result: 'certainty'
-	},
+	opacity: 0.4,
 	distinct: false
 };
 
@@ -62,6 +60,9 @@ const baseProductPropertyDefinition = {
 		color: 'red',
 		width: {
 			constant: 'cost'
+		},
+		opacity: {
+			result: 'certainty'
 		},
 		distinct: true
 	},

commit 76cf44d506a99cb82032897667bc533d35799014

Add a (broken) test that should have been added in e2d9fb1cef760c7d14978670130901957f5e279f
e2d9fb1cef760c7d14978670130901957f5e279f broke the actual default data, by tickling a long-standing bug.

This test basically just verifies the product library (which is a complex library!) works.

Part of #11.

test/base/test.js
@@ -166,6 +166,20 @@ describe('AdjacencyMap validation', () => {
 		}
 	});
 
+	it('allows product library use', async () => {
+		const input = deepCopy(legalBaseInput);
+		input.import = 'product';
+		const errorExpected = false;
+		const fn = () => {
+			new AdjacencyMap(input);
+		};
+		if (errorExpected) {
+			assert.throws(fn);
+		} else {
+			assert.doesNotThrow(fn);
+		}
+	});
+
 	it('allows base input with node with no edges', async () => {
 		const input = deepCopy(legalBaseInput);
 		delete input.nodes.a.edges;