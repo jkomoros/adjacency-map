cc717d79fec5ae99afa870df74a19a5fea776060
Alex Komoroske <jkomoros@gmail.com>
2022-11-25 09:57:15 -0500

    Pop out _svgForEdge.
    Part of #22. Part of #24.

--- src/components/main-view.ts
+++ src/components/main-view.ts
@@ -753,6 +753,10 @@ class MainView extends connect(store)(PageViewElement) {
 		return edge.edges.map(edge => edge.type).join(', ');
 	}
 
+	_svgForEdge(edge : RenderEdgeValue, map : AdjacencyMap) : TemplateResult {
+		return svg`<path d="${this._pathForEdge(edge, map)}" stroke-width='${edge.width}' stroke-opacity='${edge.opacity}' stroke='${edge.color.rgbaStr}'><title>${this._titleForEdge(edge)}</title></path>`;
+	}
+
 	_handleSVGMouseMove(e : MouseEvent) {
 		const ele = e.composedPath()[0];
 		if (!(ele instanceof Element)) throw new Error('unexpected node');
@@ -783,7 +787,7 @@ class MainView extends connect(store)(PageViewElement) {
 
 		return html`<svg @mousemove=${this._handleSVGMouseMove} @click=${this._handleSVGMouseClick} class='main' viewBox='${a.viewBox}' width='${a.width * this._scale}' height='${a.height * this._scale}' style='max-width: 100%; height: auto; height: intrinsic;' font-family='sans-serif' font-size='10'>
 			<g fill="none" stroke-linecap="${strokeLinecap}" stroke-linejoin="${strokeLinejoin}">
-				${repeat(a.renderEdges, edge => renderEdgeStableID(edge), edge => svg`<path d="${this._pathForEdge(edge, a)}" stroke-width='${edge.width}' stroke-opacity='${edge.opacity}' stroke='${edge.color.rgbaStr}'><title>${this._titleForEdge(edge)}</title></path>`)}
+				${repeat(a.renderEdges, edge => renderEdgeStableID(edge), edge => this._svgForEdge(edge, a))}
 			</g>
 			<g>
 				${Object.values(a.nodes).map(node => svg`<a transform="translate(${node.x},${node.y})">
