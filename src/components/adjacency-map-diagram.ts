0a6fb1d143b56d408a83044e2cd8a87b0c337308
Alex Komoroske <jkomoros@gmail.com>
2022-12-04 16:30:36 -0800

    Render non-top-level layoutNodes, which now renders items inside their group container.
    Part of #12.

--- src/components/adjacency-map-diagram.ts
+++ src/components/adjacency-map-diagram.ts
@@ -177,8 +177,6 @@ class AdjacencyMapDiagram extends LitElement {
 	}
 
 	_svgForNode(node : LayoutNode) : TemplateResult {
-		//For now, only render top-level nodes.
-		if (node.group) return svg``;
 		// color of label halo 
 		const halo = '#fff';
 		// padding around the labels
